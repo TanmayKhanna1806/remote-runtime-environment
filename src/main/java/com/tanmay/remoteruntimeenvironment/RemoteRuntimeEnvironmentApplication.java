c173acd7050e06fa5f91b6e5b8ccbb448f43fb9b
TanmayKhanna1806 <tanmaykhanna1806@gmail.com>
2026-08-29 16:16:13 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/tanmay/remoteruntimeenvironment/RemoteRuntimeEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.remoteruntimeenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteRuntimeEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteRuntimeEnvironmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanmay/remoteruntimeenvironment/RemoteRuntimeEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.remoteruntimeenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteRuntimeEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
